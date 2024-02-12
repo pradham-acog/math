0e509c6deb0c0710f3434d8454517bc378d448d8
pradham-acog <pradham@aganitha.ai>
2024-02-12 14:35:25 +0530

    module

---
+++ mathModule.js
@@ -0,0 +1,18 @@
+function add(x, y) {
+  return x + y;
+}
+function substract(x, y) {
+  return x - y;
+}
+function multiply(x, y) {
+  return x * y;
+}
+function divide(x, y) {
+  return x / y;
+}
+
+function squareRoot(x) {
+  return Math.sqrt(x);
+}
+module.exports = { add, substract, multiply };
+module.exports = { squareRoot };
